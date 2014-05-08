4d9095d2f4a5aa00437581254f8137a08fbebc72
Tyler Shipe <tjshipe@gmail.com>
2014-05-08 15:06:50 -0400

    blug

--- lib/search_magic/full_text_search.rb
+++ lib/search_magic/full_text_search.rb
@@ -9,7 +9,7 @@ module SearchMagic
       field :arrangeable_values, :type => Hash, :default => {}
       before_save :update_searchable_values
       before_save :update_arrangeable_values
-      after_save :update_associated_documents
+      # after_save :update_associated_documents
     end
     
     module ClassMethods
